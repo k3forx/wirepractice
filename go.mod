a5cfa2258dbb1aa760d279ae14002a0c3bcb6bf2
Kanata Miyahana <afiri.bupa@gmail.com>
2023-08-24 23:23:01 +0900

    init wire

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/k3forx/wirepractice
 
 go 1.21.0
+
+require github.com/google/wire v0.5.0
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+type Message string
+
+type Greeter struct {
+	Message Message
+}
+
+func (g Greeter) Greet() Message {
+	return g.Message
+}
+
+type Event struct {
+	Greeter Greeter
+}
+
+func (e Event) Start() {
+	msg := e.Greeter.Greet()
+	fmt.Println(msg)
+}
+
+func NewMessage() Message {
+	return Message("Hi there!")
+}
+
+func NewGreeter(m Message) Greeter {
+	return Greeter{Message: m}
+}
+
+func NewEvent(g Greeter) Event {
+	return Event{Greeter: g}
+}
+
+func main() {
+	e := InitializeEvent()
+
+	e.Start()
+}
